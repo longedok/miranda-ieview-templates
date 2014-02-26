41aea46c9e54a620ecac3881dd55c1957b10c6a5
Evgeniy Kartavykh <longedok@gmail.com>
2014-02-26 21:18:16 +0300

    basic bnw.im support was added

--- nizers.js
+++ nizers.js
@@ -1,14 +1,5 @@
-/**    
- * youtubePlayer = '<object style="height: 240px; width: 320px">' +
- *               '<param name="movie" value="{0}">'+
- *               '<param name="allowFullScreen" value="true">' +
- *               '<param name="allowScriptAccess" value="always">' + 
- *               '<embed src="{1}" type="application/x-shockwave-flash" ' +
- *               'width="320" height="240"></object>';
- */
-
 /** 
- * @description A nizer for juick.com.
+ * @description A nizer for juick.com
  *
  * @return void
  */
@@ -47,79 +38,72 @@ function juickonizer(inpId){
 }
 
 /** 
- * @description A nizer for point.im.
+ * @description A nizer for point.im
  *
  * @return void
  */
-function pstonizer(inpId){
-    var pstolink = "xmpp:p@point.im?message;body=";
-    // regexp для номеров сообщений. 
-    // пример работы:
-    // для номера #123456/1 в $1 - будет #123456/1, в $2 - #123456
+function pointizer(inpId){
+    var pointlink = "xmpp:p@point.im?message;body=";
+    // regexp for the post's ids   
+    // example:
+    // for a post id '#abcd/1' $1 match group would contain '#abcd/1' and $2 - '#abcd'
     var msgNumRegEx = /\B((#[a-z0-9]{4,})(\/\d+)?)/igm;
     var imageRegEx = /(<a[^<]*?>((?:http:\/\/)?(?:www\.)?[^\s]+?\/[^\s]+?(?:\.jpg|\.jpeg|\.gif|\.png))<\/a>)/igm;
 
     add(inpId, imageRegEx, '<br /><br /><img width="320" height="240" src="$2" /><br />');
-    // обoрачиваем номера постов/комментов ссылками
-    wrap(inpId, msgNumRegEx, '<a>', 'class="msgNum" href="' + pstolink + '$1%20"');
-    // добавляем управление вставкой номеров постов/комментариев
+    // wrap messages and comments numbers with xmpp links
+    // when user clicks the link the message number goes into input box so user could write an answer to the message
+    wrap(inpId, msgNumRegEx, '<a>', 'class="msgNum" href="' + pointlink + '$1%20"');
+
+    // add commands for requesting post's comments, subscribing to a post, etc.
     var msgNumRegEx2 = /\B(((#[a-z]{4,})(\/\d+)?)<\/a>)/igm;
     // #123456+
-    var msgNumControl = '(<a class="controls" title="Показать комментарии" href="' + pstolink + '$3+">+</a>';
+    var msgNumControl = '(<a class="controls" title="Показать комментарии" href="' + pointlink + '$3+">+</a>';
     // S #123456
-    msgNumControl += ' <a class="controls" title="Подписаться на комментарии" href="' + pstolink + 'S%20$3">S</a>';
+    msgNumControl += ' <a class="controls" title="Подписаться на комментарии" href="' + pointlink + 'S%20$3">S</a>';
     // U #123456
-    msgNumControl += ' <a class="controls" title="Отписаться от комментариев" href="' + pstolink + 'U%20$3">U</a>';
+    msgNumControl += ' <a class="controls" title="Отписаться от комментариев" href="' + pointlink + 'U%20$3">U</a>';
     // D #123456
-    msgNumControl += ' <a class="controls" title="Удалить запись" href="' + pstolink + 'D%20$2">D</a>';
-    msgNumControl += ' <a class="controls" title="Рекомендовать запись" href="' + pstolink + '!%20$3">!</a>)';
+    msgNumControl += ' <a class="controls" title="Удалить запись" href="' + pointlink + 'D%20$2">D</a>';
+    msgNumControl += ' <a class="controls" title="Рекомендовать запись" href="' + pointlink + '!%20$3">!</a>)';
 
     add(inpId, msgNumRegEx2, msgNumControl);
     
-    //// Добавляем +10 и +20 в конец каждого сообщения
+    // Add +10 and +20 at the end of each message
     document.getElementById(inpId).innerHTML += "<br><br>";
-    //document.getElementById(inpId).innerHTML += '<a class="controls"  title="Топ." href="' + pstolink + '@top+">@</a> ';
-    document.getElementById(inpId).innerHTML += '<a class="controls"  title="Показать последние 10 сообщений" href="' + pstolink + '+10">+10</a>';
-    document.getElementById(inpId).innerHTML += ' <a class="controls" title="Показать последние 20 сообщений" href="' + pstolink + '+20">+20</a> ';
+    document.getElementById(inpId).innerHTML += '<a class="controls"  title="Показать последние 10 сообщений" href="' + pointlink + '+10">+10</a>';
+    document.getElementById(inpId).innerHTML += ' <a class="controls" title="Показать последние 20 сообщений" href="' + pointlink + '+20">+20</a> ';
 }
 
 /** 
- * @description A nizer for bakanyaka.org.com.
+ * @description A nizer for bnw.im
  *
  * @return void
  */
-function nyaizer(inpId){
-    var jlink = "xmpp:nyashaj@neko.im?message;body=";
-    // regexp для номеров сообщений. 
-    // пример работы:
-    // для номера #123456/1 в $1 - будет #123456/1, в $2 - #123456
-    var msgNumRegEx = /\B((#\d+)(\/\d+)?)/igm;
-    var nickRegEx = /\B(@[a-zA-Z0-9-.@_|]+)\b/gm;
-    var tagRegEx = /(\*\S+?)(?= ?<br>| \*)/igm;
+function bnwizer(inpId){
+    var bnwlink = "xmpp:bnw@bnw.im?message;body=";
+    // regexp for the post's ids   
+    // example:
+    // for a post id '#TQIW9E/6OF' $1 match group would contain '#TQIW9E/6OF' and $2 - '#TQIW9E'
+    var msgNumRegEx = /((#[A-Z0-9]{6,})(\/[A-Z0-9]{3,})?)/igm
         
-    // обoрачиваем номера постов/комментов ссылками
-    wrap(inpId, msgNumRegEx, '<a>', 'class="msgNum" href="' + jlink + '$1%20"');
-    // оборачиваем ники ссылками
-    wrap(inpId, nickRegEx, '<a>', 'class="nick" href="' + jlink + '$1+"');
-    wrap(inpId, tagRegEx, '<a>', 'class="tag" href="' + jlink + '$1"');
-    // добавляем управление вставкой номеров постов/комментариев
-    var msgNumRegEx2 = /\B(((#\d+)(\/\d+)?)<\/a>)/igm;
+    wrap(inpId, msgNumRegEx, '<a>', 'class="msgNum" href="' + bnwlink + '$1%20"', '$1', '--- ');
+    var msgNumRegEx2 = /(((#[A-Z0-9]{6,})(\/[A-Z0-9]{3,})?)<\/a>)/igm;
     // #123456+
-    var msgNumControl = '(<a class="controls" title="Показать комментарии" href="' + jlink + '$3+">+</a>';
+    var msgNumControl = '(<a class="controls" title="Показать комментарии" href="' + bnwlink + '$3+">+</a>';
     // S #123456
-    msgNumControl += ' <a class="controls" title="Подписаться на комментарии" href="' + jlink + 'S%20$3">S</a>';
+    msgNumControl += ' <a class="controls" title="Подписаться на комментарии" href="' + bnwlink + 'S%20$3">S</a>';
     // U #123456
-    msgNumControl += ' <a class="controls" title="Отписаться от комментариев" href="' + jlink + 'U%20$3">U</a>';
+    msgNumControl += ' <a class="controls" title="Отписаться от комментариев" href="' + bnwlink + 'U%20$3">U</a>';
     // D #123456
-    msgNumControl += ' <a class="controls" title="Удалить запись" href="' + jlink + 'D%20$2">D</a>';
+    msgNumControl += ' <a class="controls" title="Удалить запись" href="' + bnwlink + 'D%20$2">D</a>';
     // ! #123456
-    msgNumControl += ' <a class="controls" title="Рекомендовать запись" href="' + jlink + '!%20$3">!</a>)';
+    msgNumControl += ' <a class="controls" title="Рекомендовать запись" href="' + bnwlink + '!%20$3">!</a>)';
 
     add(inpId, msgNumRegEx2, msgNumControl);
     
     // Добавляем # и #+ в конец каждого сообщения
     document.getElementById(inpId).innerHTML += "<br><br>";
-    document.getElementById(inpId).innerHTML += '<a class="controls"  title="Топ." href="' + jlink + '@top+">@</a> ';
-    document.getElementById(inpId).innerHTML += '<a class="controls"  title="Показать вашу ленту." href="' + jlink + '#">#</a>';
-    document.getElementById(inpId).innerHTML += ' <a class="controls" title="Показать последние 10 сообщений" href="' + jlink + '#+">#+</a> ';
+    document.getElementById(inpId).innerHTML += '<a class="controls"  title="Показать вашу ленту." href="' + bnwlink + '#">#</a>';
+    document.getElementById(inpId).innerHTML += ' <a class="controls" title="Показать последние 10 сообщений" href="' + bnwlink + '#+">#+</a> ';
 }
